5d31a0c395b707986481e96652456dbf76342499
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-02-25 09:11:19 -0500

    initial commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Thomas Cowern New on 2/25/22.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
