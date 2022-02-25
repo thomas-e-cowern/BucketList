d018cba0d989ded2dcc6ca99007fed8f8190393c
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-02-25 09:46:10 -0500

    added docuemtns directory

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -10,7 +10,26 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
-            .padding()
+            .onTapGesture {
+                let str = "Test Message"
+                
+                let url = getDocumentsDirectory().appendingPathComponent("message.txt")
+                
+                do {
+                    try  str.write(to: url, atomically: true, encoding: .utf8)
+                    
+                    let input = try String(contentsOf: url)
+                    print(input)
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
+    }
+    
+    func getDocumentsDirectory () -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        
+        return paths[0]
     }
 }
 
