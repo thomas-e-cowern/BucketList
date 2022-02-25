f03f5061c2490e4879aa4f531118349ff2e7e242
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-02-25 10:52:52 -0500

    added mapkit

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -6,8 +6,23 @@
 //
 
 import SwiftUI
+import MapKit
+
+struct Location: Identifiable {
+    let id = UUID()
+    let name: String
+    let coordinates: CLLocationCoordinate2D
+}
 
 struct ContentView: View {
+    
+    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 26.7, longitude: -80.0), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+    
+    let locations = [
+        Location(name: "Juno Beach", coordinates: CLLocationCoordinate2D(latitude: 26.8, longitude: -80.0)),
+        Location(name: "Rosemary Square", coordinates: CLLocationCoordinate2D(latitude: 26.7084, longitude: -80.0)),
+        Location(name: "Home", coordinates: CLLocationCoordinate2D(latitude: 26.726, longitude: -80.11))
+    ]
     
     var loadingState = LoadingState.success
     
@@ -20,6 +35,8 @@ struct ContentView: View {
         } else {
             FailedView()
         }
+        
+        Map(coordinateRegion: $mapRegion)
         
     }
 }
