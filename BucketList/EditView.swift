d38979c3b8a5600c1d8f3e86b431a3a264e796fa
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-02-28 07:12:42 -0500

    added fetch nearby places

--- BucketList/EditView.swift
+++ BucketList/EditView.swift
@@ -59,6 +59,9 @@ struct EditView: View {
                     dismiss()
                 }
             }
+            .task {
+                await fetchNearbyPlaces()
+            }
         }
     }
     
@@ -68,6 +71,25 @@ struct EditView: View {
         
         _name = State(initialValue: location.name)
         _description = State(initialValue: location.description)
+    }
+    
+    func fetchNearbyPlaces () async {
+        let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(location.coordinate.latitude)%7C\(location.coordinate.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
+        
+        guard let url = URL(string: urlString) else {
+            print("Bad URL")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let items = try JSONDecoder().decode(Result.self, from: data)
+            pages = items.query.pages.values.sorted { $0.title < $1.title }
+            loadingState = .loaded
+        } catch {
+            loadingState = .failed
+        }
+
     }
 }
 
